504fd630bd173d4fa8386916f7538125f7307f4b
aparkala <aparkala@scu.edu>
2021-05-13 00:02:18 -0700

    Create, View, Modify GUI

---
+++ src/ViewForm.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ViewForm {
+    private JSplitPane splitPane;
+    private JLabel groupdIDLabel;
+    private JLabel rcmLabel;
+    private JPanel viewFormPanel;
+    private JScrollPane groupScrollPane;
+    private JPanel viewRcmPane;
+}
---
+++ src/viewRCM.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class viewRCM {
+    private JPanel viewRcmPane;
+    private JButton activateDeactivateButton;
+    private JTable statusTable;
+    private JTabbedPane addModifyTab;
+    private JButton emptyButton;
+    private JTextField newItemName;
+    private JFormattedTextField newItemPrice;
+    private JButton addButton;
+    private JTextField modifiedItemName;
+    private JComboBox itemComboBox;
+    private JFormattedTextField modifiedItemPrice;
+    private JButton modifyButton;
+    private JPanel buttonPane;
+    private JSplitPane modifySplitPane;
+    private JPanel statusPane;
+    private JPanel addTab;
+    private JPanel modifyTab;
+    private JPanel addButtonPane;
+    private JPanel modifyButtonPane;
+}
